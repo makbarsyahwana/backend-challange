95aaf90029e6cdd2e2a0b7c0a2f14d29665eb035
makbarsyahwana <makbarsyahwana@gmail.com>
2018-12-25 12:48:55 +0700

    Update port

--- index.js
+++ index.js
@@ -59,4 +59,6 @@ app.get('/', (req, res) => {
   res.send("Hello World")
 })
 
-app.listen(process.env.port || 3000, () => console.log("app running on port 3000"))
+app.listen(process.env.PORT || 3000, () => { console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env); })
+
+
