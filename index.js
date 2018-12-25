48c10af6542bba14bafe9b31ee9d48b0f9965e7b
makbarsyahwana <makbarsyahwana@gmail.com>
2018-12-25 12:43:32 +0700

    Update port

--- index.js
+++ index.js
@@ -59,4 +59,4 @@ app.get('/', (req, res) => {
   res.send("Hello World")
 })
 
-app.listen(3000, () => console.log("app running on port 3000"))
+app.listen(process.env.port || 3000, () => console.log("app running on port 3000"))
